b56e7494adfab2619f8b806301ff6d3261712d82
Yich <vi000246@gmail.com>
2019-04-16 11:52:27 +0800

    123

--- RescueTime-SaveBusyDude/Form1.Designer.cs
+++ RescueTime-SaveBusyDude/Form1.Designer.cs
@@ -28,8 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "程式已在背景執行!";
+            this.notifyIcon1.BalloonTipTitle = "SaveBusyDude";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClicked += new System.EventHandler(this.notifyIcon1_BalloonTipClicked);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
@@ -38,12 +64,19 @@
             this.ClientSize = new System.Drawing.Size(120, 0);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "RescueTime-SaveBusyDude";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- RescueTime-SaveBusyDude/Form1.cs
+++ RescueTime-SaveBusyDude/Form1.cs
@@ -12,9 +12,68 @@ namespace RescueTime_SaveBusyDude
 {
     public partial class Form1 : Form
     {
+        #region 視窗控制
         public Form1()
         {
             InitializeComponent();
+            //加入結束按鈕
+            ToolStripItem btnClose = this.contextMenuStrip1.Items.Add("結束");
+            btnClose.Click += new EventHandler(btnClose_Click);
+            //氣泡提示popup
+            notifyIcon1.ShowBalloonTip(3000, this.Text,
+                "程式已在背景執行!",
+                ToolTipIcon.Info);
+        }
+        void btnClose_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            Close();
+        }
+
+        //托盤icon點擊
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ShowForm();
+        }
+        //氣泡圖示點擊
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            ShowForm();
+        }
+        /// <summary>
+        /// 顯示出主畫面
+        /// </summary>
+        private void ShowForm()
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                //如果目前是縮小狀態，才要回覆成一般大小的視窗
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+            // Activate the form.
+            this.Activate();
+            this.Focus();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.WindowState != FormWindowState.Minimized)
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;
+                notifyIcon1.Tag = string.Empty;
+                notifyIcon1.ShowBalloonTip(3000, this.Text,
+                    "程式並未結束，要結束請在圖示上按右鍵，選取結束功能!",
+                    ToolTipIcon.Info);
+            }
+        }
+        #endregion
+
+        //定期執行method
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
