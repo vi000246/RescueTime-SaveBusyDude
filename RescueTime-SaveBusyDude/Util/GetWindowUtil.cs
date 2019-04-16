6187cdb807f9c243b00a270c78ff2623206811ff
Yich <vi000246@gmail.com>
2019-04-16 17:48:14 +0800

    123

--- RescueTime-SaveBusyDude/Util/GetWindowUtil.cs
+++ RescueTime-SaveBusyDude/Util/GetWindowUtil.cs
@@ -16,27 +16,36 @@ namespace rescuetime_savebusydude
     /// </summary>
     public static class WindowUtil
     {
+        private static Process _realProcess;
+
         // 會用到的 WinAPI
-//        [DllImport("user32.dll")]
-//        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpsb, int count);
+        //        [DllImport("user32.dll")]
+        //        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpsb, int count);
         [DllImport("user32.dll")]
         private static extern IntPtr GetForegroundWindow();
         [DllImport("user32.dll")]
         public static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
+        //Used to get ID of any Window
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
+        public delegate bool WindowEnumProc(IntPtr hwnd, IntPtr lparam);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumChildWindows(IntPtr hwnd, WindowEnumProc callback, IntPtr lParam);
 
         //取得目前視窗標題
-//        public static string GetCurrentWindowTitle()
-//        {
-//            const int nChars = 256;
-//            StringBuilder Buff = new StringBuilder(nChars);
-//            IntPtr handle = GetForegroundWindow();
-//
-//            if (GetWindowText(handle, Buff, nChars) > 0)
-//            {
-//                return Buff.ToString();
-//            }
-//            return null;
-//        }
+        //        public static string GetCurrentWindowTitle()
+        //        {
+        //            const int nChars = 256;
+        //            StringBuilder Buff = new StringBuilder(nChars);
+        //            IntPtr handle = GetForegroundWindow();
+        //
+        //            if (GetWindowText(handle, Buff, nChars) > 0)
+        //            {
+        //                return Buff.ToString();
+        //            }
+        //            return null;
+        //        }
 
         //取得目前視窗的process name
         public static string GetActiveProcessName()
@@ -45,6 +54,11 @@ namespace rescuetime_savebusydude
             uint pid;
             GetWindowThreadProcessId(hwnd, out pid);
             Process p = Process.GetProcessById((int)pid);
+            //如果是windows市集的應用程式 取得真正的名稱
+            if (p.ProcessName == "ApplicationFrameHost")
+            {
+                p = GetRealProcess(p);
+            }
             return p.ProcessName;
         }
 
@@ -74,7 +88,26 @@ namespace rescuetime_savebusydude
                 return null;
             }
         }
-
+        private static Process GetRealProcess(Process foregroundProcess)
+        {
+            EnumChildWindows(foregroundProcess.MainWindowHandle, ChildWindowCallback, IntPtr.Zero);
+            return _realProcess;
+        }
+        private static bool ChildWindowCallback(IntPtr hwnd, IntPtr lparam)
+        {
+            var process = Process.GetProcessById(GetWindowProcessId(hwnd));
+            if (process.ProcessName != "ApplicationFrameHost")
+            {
+                _realProcess = process;
+            }
+            return true;
+        }
+        public static int GetWindowProcessId(IntPtr hwnd)
+        {
+            int pid;
+            GetWindowThreadProcessId(hwnd, out pid);
+            return pid;
+        }
         private static string GetChromeUrl(Process process)
         {
             if (process == null)
